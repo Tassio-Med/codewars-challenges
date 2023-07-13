5989c5bd79a6d1fc2dd666cd2ee090e268fff3b3
Tassio-Med <tassiohfmed@gmail.como>
2023-07-13 11:08:55 -0300

    feat:challenge 04, adicionado e colocado em prática

---
+++ typescript/challenge04.ts
@@ -0,0 +1,18 @@
+// Data: 13/07/2023
+
+// Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+// Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+// Create a function which translates a given DNA string into RNA.
+
+// For example:
+
+// "GCAT"  =>  "GCAU"
+
+
+// ==> My solution:
+
+export function DNAtoRNA(dna: string): string {
+  return dna.replace(/T/gi, 'U');
+}
