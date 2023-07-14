ee15957c491bd390213e7232a2f587c7ad5f19bf
Tassio-Med <tassiohfmed@gmail.como>
2023-07-14 12:13:29 -0300

    feat: challenge 08 colocado em prática.

---
+++ typescript/challenge08.ts
@@ -0,0 +1,11 @@
+// Data: 14/07/2023
+
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+// ==> My solution:
+
+export const booleanToString = (b:boolean):string => {
+  return b === true ? "true" : "false" ;
+};
