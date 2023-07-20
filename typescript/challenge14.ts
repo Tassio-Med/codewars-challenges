93d79ac47ea46f52c23e27e58a0893f83743988c
Tassio-Med <tassiohfmed@gmail.como>
2023-07-20 20:55:34 -0300

    fix: correção de data

--- typescript/challenge14.ts
+++ typescript/challenge14.ts
@@ -1,4 +1,4 @@
-// Data: 18/07/2023
+// Data: 19/07/2023
 
 // A square of squares
 // You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
