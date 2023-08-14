8df773d0af854b2e04c1dbc5ca82fd6fd50acfca
Tassio-Med <tassiohfmed@gmail.como>
2023-08-14 11:05:40 -0300

    fix: correção de data

--- typescript/challenge28.ts
+++ typescript/challenge28.ts
@@ -1,4 +1,4 @@
-// Data: 09/07/2023
+// Data: 09/08/2023
 
 // You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
 
--- typescript/challenge29.ts
+++ typescript/challenge29.ts
@@ -1,4 +1,4 @@
-// Data: 14/07/2023
+// Data: 10/08/2023
 
 // Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
 
