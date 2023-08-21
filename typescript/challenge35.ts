da3f2e9b4838286772dc1a39a172f9b5c790ca14
Tassio-Med <tassiohfmed@gmail.como>
2023-08-21 15:37:09 -0300

    feat: challenge 35, colocado em prática

---
+++ typescript/challenge35.ts
@@ -0,0 +1,13 @@
+// Data: 21/08/2023
+
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// Example output:
+
+// Hello, Mr. Spock
+
+// ==> My solution:
+
+export function sayHello(name: string): string {
+  return `Hello, ${name}`;
+}
