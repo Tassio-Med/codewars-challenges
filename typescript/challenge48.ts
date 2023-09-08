14e8c6c7c22f90f980d275b6ec33b16d51a970ac
Tassio-Med <tassiohfmed@gmail.como>
2023-09-08 11:15:03 -0300

    feat: challenge 48 colocado em prática

---
+++ typescript/challenge48.ts
@@ -0,0 +1,13 @@
+// Data: 08/09/2023
+
+// Write a function to split a string and convert it into an array of words.
+
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+
+// ==> My solution:
+
+export const stringToArray = (s: string): string[] => s.split(" ");
