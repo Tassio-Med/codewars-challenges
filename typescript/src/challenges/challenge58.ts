005750cf255ac38fa52a8184e02b1b1d7611071f
Tassio-Med <tassiohfmed@gmail.como>
2023-09-21 17:03:38 -0300

    feat: descrição do desafio58 adicionada

--- typescript/src/challenges/challenge58.ts
+++ typescript/src/challenges/challenge58.ts
@@ -1,3 +1,16 @@
+// Data: 21/09/2023
+
+// Challenge:
+
+// Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+
+// Example:
+
+// Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+
+// ==> My solution:
+
 export function flattenAndSort(inputArray: number[][]): number[] {
   const newArray: number[] = [];
   inputArray.forEach((arrayElement) => {
