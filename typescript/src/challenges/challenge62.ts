5d1b1d2862838cd35f50f102988171de5831f0e1
Tassio-Med <tassiohfmed@gmail.como>
2023-09-26 15:48:58 -0300

    feat: challenge62 adicionado e colocado em prática

---
+++ typescript/src/challenges/challenge62.ts
@@ -0,0 +1,17 @@
+// Data: 26/09/2023
+
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+// ==> My solution:
+
+export class Kata {
+  static opposite(n: number) {
+    return n * -1
+  }
+}
