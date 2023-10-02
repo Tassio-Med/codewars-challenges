028fb8a89d9bc2926ac96b18e7c6e1955a92956b
Tassio-Med <tassiohfmed@gmail.como>
2023-10-02 15:45:54 -0300

    feat: challenge65 adicionado e colocado em prática

---
+++ typescript/src/challenges/challenge65.ts
@@ -0,0 +1,9 @@
+// Data: 02/10/2023
+
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// ==> My Solution:
+
+export function square(num: number): number {
+  return num ** 2;
+}
