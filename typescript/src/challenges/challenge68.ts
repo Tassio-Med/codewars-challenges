27e973285c944e04825dd10d497e9c2b388161f9
Tassio-Med <tassiohfmed@gmail.como>
2023-10-05 10:42:06 -0300

    feat: challenge68 adicionado e colocado em pŕatica

---
+++ typescript/src/challenges/challenge68.ts
@@ -0,0 +1,13 @@
+// Data: 05/10/2023
+
+// Input: Array of elements
+
+// ["h","o","l","a"]
+
+// Output: String with comma delimited elements of the array in th same order.
+
+// "h,o,l,a"
+
+// ==> My solution:
+
+export const printArray = (array: any[]) =>  array.join(",");
