c1eebe64507e6e16dc0337b7407828e4aef12e25
Tassio-Med <tassiohfmed@gmail.como>
2024-03-12 11:30:05 -0300

    feat: challenge02 resolvido

---
+++ C#/challenges/challenge02.cs
@@ -0,0 +1,21 @@
+// Data: 12/03/2024
+
+/* In this Kata we are passing a number (n) into a function.
+
+Your code will determine if the number passed is even (or not).
+
+The function needs to return either a true or false.
+
+Numbers may be positive or negative, integers or floats.
+
+Floats with decimal part non equal to zero are considered UNeven for this kata. */
+
+// ==> My solution:
+
+public class Number
+{
+  public bool IsEven(double n) => n % 2 == 0;
+  
+}
+
+
