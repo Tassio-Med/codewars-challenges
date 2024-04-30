2f76440a785d3bbd3e9398fc7d020e50272594d6
Tassio-Med <tassiohfmed@gmail.como>
2024-04-30 16:06:28 -0300

    feat: challenge09 resolvido

---
+++ C#/challenges/challenge09.cs
@@ -0,0 +1,30 @@
+// Data: 30/04/2024
+
+/* Description
+We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+Examples
+"1234" --> 1234
+"605"  --> 605
+"1405" --> 1405
+"-7" --> -7 */
+
+// ==> My solution:
+
+using System;
+  public class Kata
+  {
+    public static int StringToNumber(String str) {
+        return int.Parse(str);
+  }
+}
+
+/* --> OUTRAS SOLUÇÕES INTERESSANTES:
+
+
+ */
+
+
+
