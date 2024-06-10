82d5cb8161534cdde44f0b4155475da30941b316
Tassio-Med <tassiohfmed@gmail.como>
2024-06-10 10:40:30 -0300

    feat: challenge83 colocado em prática

---
+++ typescript/src/challenges/challenge83.ts
@@ -0,0 +1,16 @@
+// Data: 10/06/2024
+/* 
+Complete the function which converts hex number (given as a string) to a decimal number.
+*/
+
+// ==> My Solution:
+
+export function hexToDec(hexString: string): number {
+  return parseInt(hexString, 16);
+}
+
+// ==> OUTRAS ALTERNATIVAS DE RESPOSTA:
+
+/* 
+
+ */
